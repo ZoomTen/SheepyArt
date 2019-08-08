659a19ab7ef81572cc62dee49edb48949db6d0d6
ZoomTen <zy.impossible@gmail.com>
2019-08-08 14:17:07 +0700

    feat(delete): make art deletion slightly more RESTful
    Use the HTTP 'DELETE' method instead of POSTing to a /delete/ endpoint.
    This approach is not NoScript friendly as HTML doesn't support 'DELETE'
    methods, which is why a lot of RESTful APIs aren't really RESTful. Might
    revert to the old approach later on.

--- sheepyart/app/routes/delete.py
+++ sheepyart/app/routes/delete.py
@@ -2,29 +2,36 @@
 from flask import Blueprint
 from flask import render_template, request, redirect, flash, url_for, abort
 from flask_login import login_required, current_user
+from flask import Response
 
 from sheepyart.sheepyart import db
 from sheepyart.app.models import Art
 
+from os import path, remove
 delete = Blueprint('delete', __name__)
 
 
-@delete.route('/delete', methods=['POST'])
+@delete.route('/art/<art_id>', methods=['DELETE'])
 @login_required
-def delete_art():
+def delete_art(art_id):
     # Check if the owner of the art is the same
-    art = None
-    art_target = request.form.get('art')
-    if art_target:
-        art = Art.query.get(art_target)
+    if art_id:
+        art = Art.query.get(art_id)
 
     if art:
         if (art.by == current_user):
+            # FIXME: delete: a safer way to find static abs. path
+            image_file = path.join(path.realpath('.'), 'sheepyart', 'static', 'uploads', art.image)
+            thumb_file = path.join(path.realpath('.'), 'sheepyart', 'static', 'thumbnail', art.thumbnail)
             try:
                 db.session.delete(art)
                 db.session.commit()
             except:
                 abort(500)
-            return redirect(url_for('userpage.view_userpage',
-                                    username=current_user.username))
+            try:
+                remove(image_file)
+                remove(thumb_file)
+            except:
+                return Response('Delete succeeded; however the files are still on our servers. Contact us for deletion!', 200)
+            return Response('Delete succeeded.', 200)
     abort(403)
