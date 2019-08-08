591c68ebeb45a8a1484c8535d0ee0f9e7a6a01a8
ZoomTen <zy.impossible@gmail.com>
2019-08-08 17:52:40 +0700

    feat(collection): implement removing from collection

--- sheepyart/app/routes/collections.py
+++ sheepyart/app/routes/collections.py
@@ -20,13 +20,12 @@ def add_to_collection(collection, art, add_collection_name=True):
         add_collection_name(Bool)
     """
     if collection:
-        collection_owner_id = collection.user_id
         exists = CollectionData.query.filter_by(art_id=art.id, collection_id=collection.id).count()
         if exists > 0:
             return Response(f'Art already exists!', 500)
         else:
             if art:
-                if (current_user.id == collection_owner_id):
+                if (current_user.id == collection.user_id):
                     collection_entry = CollectionData(art_id=art.id,
                                                       collection_id=collection.id)
                     try:
@@ -44,7 +43,34 @@ def add_to_collection(collection, art, add_collection_name=True):
     return Response('No collection specified!', 403)
 
 
-@collection.route('/add_collection', methods=['POST'])
+def delete_from_collection(collection, art, rm_collection_name=True):
+    """Delete from collection
+
+    Parameters:
+        collection(CollectionMeta)
+        art(Art)
+        add_collection_name(Bool)
+    """
+    if collection:
+        if art:
+            if (current_user.id == collection.user_id):
+                entry = CollectionData.query.filter_by(art_id=art.id,collection_id=collection.id).first()
+                try:
+                    db.session.delete(entry)
+                    db.session.commit()
+                except:
+                    Response('There was an error with deleting from the collection', 500)
+                # FIXME: collections: make better responses
+                if rm_collection_name:
+                    return Response(f"{art.title} successfully removed from {collection.title}", 200)
+                else:
+                    return Response(f'Successfully removed from favorites!', 200)
+            return Response("You don't own this collection", 403)
+        return Response('No art specified!', 403)
+    return Response('No collection specified!', 403)
+
+
+@collection.route('/add_to_collection', methods=['POST'])
 @login_required
 def user_add_collection():
     collection = None
@@ -72,5 +98,29 @@ def user_add_favorite():
 
     return add_to_collection(collection, art, add_collection_name=False)
 
+@collection.route('/remove_from_collection', methods=['POST'])
+@login_required
+def user_rm_collection():
+    collection = None
+    art = None
+    collection_id = request.form.get('collection')
+    if collection_id:
+        collection = CollectionMeta.query.get(collection_id)
+    art_id = request.form.get('art')
+    if art_id:
+        art = Art.query.get(art_id)
 
-# FIXME: collections: implement collection removing.
+    return delete_from_collection(collection, art)
+
+
+@collection.route('/remove_favorite', methods=['POST'])
+@login_required
+def user_rm_favorite():
+    # Get user's favorites table
+    collection = CollectionMeta.query.filter_by(use_as_favorites=1, user_id=current_user.id).first()
+    art = None
+    art_id = request.form.get('art')
+    if art_id:
+        art = Art.query.get(art_id)
+
+    return delete_from_collection(collection, art, rm_collection_name=False)
